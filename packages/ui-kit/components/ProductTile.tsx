4b9f9f3599e4ebce22a5f60618e314b947dc6a29
Prathamesh <pprathamesh98@gmail.com>
2026-01-26 02:25:39 +0530

    feat(consumer-app): Home Listings Screen
    Implemented Latest, Recently Viewed, and Recommended sections on Home Screen.
    
    For: NO-TICKET

--- packages/ui-kit/components/ProductTile.tsx
+++ packages/ui-kit/components/ProductTile.tsx
@@ -259,7 +259,7 @@ export const ProductTile = React.forwardRef<TamaguiElement, ProductTileProps>(
                                     source={{ uri: imageUrl }}
                                     // @ts-ignore
                                     src={imageUrl}
-                                    resizeMode="cover"
+                                    style={{ objectFit: 'cover' } as any}
                                 />
 
                                 {isOnSale && (
