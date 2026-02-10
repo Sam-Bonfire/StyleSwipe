28ae986d5394a79c56d2118ab4738d71ff964237
Sam <pprathamesh98@gmail.com>
2026-02-11 01:34:07 +0530

    feat: Created CI/CD pipelines for the project

--- apps/consumer-app/src/screens/profile/FeedbackScreen.tsx
+++ apps/consumer-app/src/screens/profile/FeedbackScreen.tsx
@@ -80,7 +80,7 @@ export function FeedbackScreen() {
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
-            <XStack alignItems="center" space="$4" padding="$4" borderBottomWidth={1} borderColor="$borderColor">
+            <XStack alignItems="center" space="$2" padding="$2" borderBottomWidth={1} borderColor="$borderColor">
                 <TopBarIconButton
                     onPress={() => navigation.goBack()}
                     backgroundColor="$background"
