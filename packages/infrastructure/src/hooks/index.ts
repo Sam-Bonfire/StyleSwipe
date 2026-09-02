89581b38427a2c56106b4c6a9e15f93f59ea88ba
Sam <Sam-Bonfire@users.noreply.github.com>
2026-09-03 02:31:04 +0530

    fix: export useUpdatePushToken and fix SortOption type errors

--- packages/infrastructure/src/hooks/index.ts
+++ packages/infrastructure/src/hooks/index.ts
@@ -20,7 +20,7 @@ export {
   useClearCart,
 } from './useCart';
 export { useMyFeedback, useCreateFeedback, useGenerateUploadUrl } from './useFeedback';
-export { useUpdateUser, useUpdateStyleProfile } from './useUsers';
+export { useUpdateUser, useUpdateStyleProfile, useUpdatePushToken } from './useUsers';
 export * from './useOnboarding';
 export * from './useAnalytics';
 export * from './usePartnerSync';
